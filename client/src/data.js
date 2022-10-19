b73d65cf95a2919856159b00a6bef76bc916f261
nadim1212 <nadimniloy.swe@gmail.com>
2022-10-19 11:15:26 +0600

    bakery updated

---
+++ client/src/data.js
@@ -0,0 +1,79 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://i.ibb.co/2ZrVNzc/cake-png-transparent-cake-images-pluspng-21.png",
+      title: "The Cake You love",
+      desc: "GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/2ZrVNzc/cake-png-transparent-cake-images-pluspng-21.png",
+      title: "The Cake You love",
+      desc: "GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/2ZrVNzc/cake-png-transparent-cake-images-pluspng-21.png",
+      title: "The Cake You love",
+      desc: "GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://i.ibb.co/XsV2DTg/croissants-various-bakery-products-bread-67105813.jpg",
+      title: "Biscuit",
+      cat:"women"
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/1L708F4/Red-Velvet-Cup-Cake1.webp",
+      title: "Cake",
+      cat:"coat"
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/L8VB62R/images.jpg",
+      title: "Bread",
+      cat:"jeans"
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:2,
+      img:"https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:4,
+      img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+      id:7,
+      img:"https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+  ]
